425e7324bf645b3b261c9bdfc503f66bd6e5f843
DANIEL GALLAGHER - STUDENT <G00360986@gmit.ie>
2019-03-06 17:46:51 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,27 @@ export class HomePage {
 
   }
 
+  openUlsterPage(){
+    this.navCtrl.push('UlsterPage');
+
+  }//Ulster Page
+
+  openMunsterPage(){
+
+    this.navCtrl.push('MunsterPage');
+
+  }//Munster Page
+
+  openConnaughtPage(){
+
+    this.navCtrl.push('ConnaughtPage');
+
+  }//Connaught Page
+
+  openLeinsterPage(){
+
+    this.navCtrl.push('LeinsterPage');
+
+  }//Connaught Page
+
 }
